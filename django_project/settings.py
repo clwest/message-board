c4b0e8bcb2c410ecf07f7c17970c79f175200f5b
Chris West <chriswest@Chriss-MacBook-Pro.local>
2022-12-22 13:43:42 -0700

    second

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h)scdujnb&t4rx932*ef_1$^gxis2ra(^xadza6zy_9_q&*b01
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herkoapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
